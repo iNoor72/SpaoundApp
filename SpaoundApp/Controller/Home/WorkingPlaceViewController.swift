6e2465c6a6fb74ca0217a6e61670bdc1f9bef1f1
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-11 22:30:41 +0200

    Finished the design of the working space UI, added a ViewController for that view.

--- SpaoundApp/Controller/Home/WorkingPlaceViewController.swift
+++ SpaoundApp/Controller/Home/WorkingPlaceViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 class WorkingPlaceViewController: UIViewController {
     @IBOutlet var workingImage: UIImageView!
     @IBOutlet var bookingButton: UIButton!
+    @IBOutlet var scrollView: UIScrollView!
     
     override func viewDidLoad() {
+        scrollView.alwaysBounceVertical = true
+//        scrollView.showsHorizontalScrollIndicator = false
     }
 
     @IBAction func bookingBottonTapped(_ sender: UIButton) {
