77c933dae24ea568c4269d23b43e44d97a3a5120
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-11 21:30:28 +0200

    Added the design for the working space UI, finalized the working with the API and network calling and it's working, restructured some of the ViewControllers.

--- SpaoundApp/Controller/Home/HomeViewController.swift
+++ SpaoundApp/Controller/Home/HomeViewController.swift
@@ -13,6 +13,9 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var usernameLabel: UILabel!
     
+    var popularWorkingSpaces : WorkingSpace?
+    var recommendedWorkingSpaces : WorkingSpace?
+    
     override func viewDidLoad() {
         tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "TVCell")
         collectionView.register(UINib(nibName: "CustomCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CVCell")
@@ -21,24 +24,55 @@ class HomeViewController: UIViewController {
         fetchRecommendedSpaces()
     }
     
+    func fetchPopularSpaces() {
+        let url = Router.popularPlaces
+        AF.request(url).responseDecodable { [weak self](response: (DataResponse<WorkingSpace, AFError>)) in
+            switch response.result {
+            case .success(let data):
+                print("succes")
+                print(data)
+                self?.popularWorkingSpaces = data
+                self?.tableView.reloadData()
+                
+            case .failure(let error):
+                print("error with network")
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+
+    func fetchRecommendedSpaces() {
+        let url = Router.recommendedPlaces
+            AF.request(url).responseDecodable { [weak self] (response: (DataResponse<WorkingSpace, AFError>)) in
+            switch response.result {
+            case .success(let data):
+                self?.recommendedWorkingSpaces = data
+                self?.collectionView.reloadData()
+                
+            case .failure(let error):
+                print("error with network")
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
-var popularWorkingSpaces : WorkingSpace?
-var recommendedWorkingSpaces : WorkingSpace?
+
 
 //MARK:- TableView Functions
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("\(recommendedWorkingSpaces?.places.count)" + " I'm in rows")
-        return recommendedWorkingSpaces?.places.count ?? 0
+        //print("\(popularWorkingSpaces?.places.count)" + " I'm in rows")
+        return popularWorkingSpaces?.places.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TVCell", for: indexPath) as! CustomTableViewCell
         
-        cell.nameLabel.text = recommendedWorkingSpaces?.places[indexPath.row].name
-        cell.addressLabel.text = recommendedWorkingSpaces?.places[indexPath.row].address
-        cell.priceLabel.text = "\(String(describing: recommendedWorkingSpaces?.places[indexPath.row].price))"
+        cell.nameLabel.text = popularWorkingSpaces?.places[indexPath.row].name
+        cell.addressLabel.text = popularWorkingSpaces?.places[indexPath.row].address
+        cell.priceLabel.text = "\(popularWorkingSpaces?.places[indexPath.row].price ?? 0)"
         
         return cell
     }
@@ -48,57 +82,19 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 //MARK:- CollectionView Functions
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return popularWorkingSpaces?.places.count ?? 0
+        return recommendedWorkingSpaces?.places.count ?? 0
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CVCell", for: indexPath) as! CustomCollectionViewCell
         
-        cell.nameLabel.text = popularWorkingSpaces?.places[indexPath.row].name
-        cell.priceLabel.text = "\(String(describing: popularWorkingSpaces?.places[indexPath.row].price))"
+        cell.nameLabel.text = recommendedWorkingSpaces?.places[indexPath.row].name
+        cell.priceLabel.text = "\(recommendedWorkingSpaces?.places[indexPath.row].price ?? 0)"
         cell.firstFeatureLabel.text = "Comfort"
         cell.secondFeatureLabel.text = "Comfort"
-        
+    
         return cell
     }
     
-    //MARK:- Fetching Data Functions
-    
-    func fetchPopularSpaces() {
-        let url = Router.popularPlaces
-        
-        //always gives an error!
-        AF.request(url).responseDecodable { (response: (DataResponse<WorkingSpace, AFError>)) in
-            switch response.result {
-            case .success(let data):
-                print("succes")
-                print(data)
-                popularWorkingSpaces = data
-                self.tableView.reloadData()
-                
-            case .failure(let error):
-                print("error with network")
-                print(error.localizedDescription)
-            }
-            
-            
-        }
-    }
-    
 }
-
-func fetchRecommendedSpaces() {
-    let url = Router.recommendedPlaces
-    
-    //always gives an error!
-    AF.request(url).responseDecodable { (response: (DataResponse<WorkingSpace, AFError>)) in
-        switch response.result {
-        case .success(let data):
-            recommendedWorkingSpaces = data
-        case .failure(let error):
-            print("error with network")
-            print(error.localizedDescription)
-        }
-    }
-}
-
---
+++ SpaoundApp/Controller/Home/WorkingPlaceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WorkingPlaceViewController.swift
+//  SpaoundApp
+//
+//  Created by Noor Walid on 11/04/2021.
+//
+
+import UIKit
+
+class WorkingPlaceViewController: UIViewController {
+    @IBOutlet var workingImage: UIImageView!
+    @IBOutlet var bookingButton: UIButton!
+    
+    override func viewDidLoad() {
+    }
+
+    @IBAction func bookingBottonTapped(_ sender: UIButton) {
+    }
+}
